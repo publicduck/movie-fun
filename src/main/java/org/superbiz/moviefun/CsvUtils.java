94d6eb775acf83313fb6bd8fbce15eb0e2cfef27
publicduck <>
2019-11-30 17:07:31 +0100

    Removing Reads from the File System

--- src/main/java/org/superbiz/moviefun/CsvUtils.java
+++ src/main/java/org/superbiz/moviefun/CsvUtils.java
@@ -3,28 +3,34 @@ package org.superbiz.moviefun;
 import com.fasterxml.jackson.databind.MappingIterator;
 import com.fasterxml.jackson.databind.ObjectReader;
 
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class CsvUtils {
+//    Move both .csv files to the classpath by putting them into the resources folder. DONE
+//
+//    Update the readFile function in CsvUtils to load files from the classpath:
+//
+//    Obtain an instance of ClassLoader via the getClassLoader function.
+//    Update the scanner so it takes an instance of InputStream obtained from ClassLoader.getResourceAsStream instead of a new File.
 
     public static String readFile(String path) {
-        try {
-            Scanner scanner = new Scanner(new File(path)).useDelimiter("\\A");
+        ClassLoader classloader = CsvUtils.class.getClassLoader();
 
-            if (scanner.hasNext()) {
-                return scanner.next();
-            } else {
-                return "";
-            }
 
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
+        //Scanner scanner = new Scanner(new File(path)).useDelimiter("\\A");
+        InputStream input = classloader.getResourceAsStream(path);
+        Scanner scanner = new Scanner(input).useDelimiter("\\A");
+
+        if (scanner.hasNext()) {
+            return scanner.next();
+        } else {
+            return "";
         }
+
     }
 
     public static <T> List<T> readFromCsv(ObjectReader objectReader, String path) {
